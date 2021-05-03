ef79b5f27fb5d8773f287a36bdbeaeda092626ba
Justin <jmp86@yahoo.com>
2021-05-02 19:01:05 -1000

    Completed Assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
+  const formSubmit = document.getElementById('create-task-form');
+  const taskList = document.getElementById('tasks');
+  
+
+  formSubmit.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const newTask = e.target.querySelector('#new-task-description').value;
+    const taskItem = document.createElement('li');
+
+    taskItem.textContent = newTask;
+    taskList.append(taskItem)
+    
+  });
+
+  
 });
